ed4c0273a4f83cb196c785aaf0d3dce244a475dd
Helena <helena@klarr.se>
2024-05-21 13:58:35 +0200

    added image example

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,11 +1,21 @@
 import { useState } from "react";
 import TrashIcon from "./TrashIcon";
+import { todoImages } from "../data/todoImg";
 
 const TodoItem = ({ todo, onCheckboxChange }) => {
   //const [task, setTask] = useState("Study for exam");
 
+  const todoImage = todoImages.find((image) => image.id === todo.id);
+
   return (
     <div className="todo-item">
+      {todoImage && (
+        <img
+          src={todoImage.src}
+          style={{ width: "10rem", height: "7rem" }}
+          alt={`Todo ${todo.id}`}
+        />
+      )}
       <label className="checkbox-container">
         <input
           type="checkbox"
---
+++ src/data/todoImg.js
@@ -0,0 +1,22 @@
+export const todoImages = [
+  {
+    id: "66422e336a19d00ab9de1f56",
+    src: "/aaron-burden-QJDzYT_K8Xg-unsplash.jpg",
+  },
+  {
+    id: "66422e3b6a19d00ab9de1f57",
+    src: "annie-spratt-5TfCI4nj6B4-unsplash.jpg",
+  },
+  {
+    id: "66422e3f6a19d00ab9de1f58",
+    src: "/jason-goodman-vbxyFxlgpjM-unsplash.jpg",
+  },
+  {
+    id: "66422e4e6a19d00ab9de1f59",
+    src: "/kelly-sikkema-IZOAOjvwhaM-unsplash.jpg",
+  },
+  {
+    id: "6642300e6a19d00ab9de1f5b",
+    src: "/nrd-D6Tu_L3chLE-unsplash.jpg",
+  },
+];
